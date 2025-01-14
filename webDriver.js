63a1a2417069f0f4872b99bee468c74c5cd20989
Amar Patil <amarpatil2253@gmail.com>
2025-01-14 22:11:32 +0530

    make code headless

--- webDriver.js
+++ webDriver.js
@@ -1,10 +1,21 @@
 // webdriverManager.js
 const { Builder } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
 let driverInstance = null;
 
 async function getWebDriver() {
     if (!driverInstance) {
-        driverInstance = await new Builder().forBrowser("chrome").build();
+        const options = new chrome.Options();
+        options.addArguments("--headless"); // Enable headless mode
+        options.addArguments("--disable-gpu"); // Disable GPU acceleration
+        options.addArguments("--no-sandbox"); // Bypass OS security model, for testing
+        options.addArguments("--disable-dev-shm-usage"); // Overcome resource limitations on some systems
+
+        driverInstance = await new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(options)
+            .build();
     }
     return driverInstance;
 }
